76d4b12bed77cb673b93a4c52891560997ed30a8
Tuhil <Tuhil>
2024-01-25 22:40:52 +0530

    solved qn

---
+++ practice/vasilije_in_cacak.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define tc        \
+    long long _t; \
+    cin >> _t;    \
+    while (_t--)
+typedef long long ll;
+#define int long long
+
+// type name short forms
+#define vvll vector<vector<long long>>
+#define vll vector<long long>
+#define pll pair<long long, long long>
+
+#define loop(_k) for (int _q = 0; _q < _k; _q++)
+#define arrinp(_a, _n)                      \
+    _a.resize(_n);                          \
+    for (long long _l = 0; _l < _n; _l++) { \
+        cin >> _a[_l];                      \
+    }
+
+#define debug(x) cerr << (#x) << " " << (x) << endl
+
+signed main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    tc {
+        ll n, k, x;
+        cin >> n >> k >> x;
+        ll minim = (k * (k + 1)) / 2;
+        ll maxim = (n - k) * k + minim;
+
+        if (x < minim || x > maxim)
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+    }
+}
